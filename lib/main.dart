4aece834515f826b8e0143b65555d2b060c24809
teejstroyer <titoriano@gmail.com>
2020-11-15 10:10:22 -0600

    prevent null exception

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 void main() {
   var dung = Dungeon();
   dung.generate(10);
-  dung.rooms[Random().nextInt(dung.rooms.length)].current = true;
+  //dung.rooms[Random().nextInt(dung.rooms.length)].current = true;
   dung.rooms[Random().nextInt(dung.rooms.length)].visited = true;
 
   runApp(
@@ -21,13 +21,18 @@ void main() {
 }
 
 Container getMiniMap(Dungeon dung, double squareSize) {
-  var currentRoom = dung.rooms.firstWhere((element) => element.current);
+  var currentRoom = dung.rooms.any((i) => i.current) ? dung.rooms.firstWhere((element) => element.current) : dung.rooms.first;
   double miniMapSize = 4 * squareSize;
   double offsetConstant = 1.5 * squareSize;
   var mapOffset = Matrix4.translationValues(-(squareSize * currentRoom.x - offsetConstant), -(squareSize * currentRoom.y - offsetConstant), 0);
 
   return Container(
-    decoration: BoxDecoration(border: Border.all(color: Colors.black), color: Colors.transparent),
+    decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black,
+          width: 3,
+        ),
+        color: Colors.transparent),
     height: miniMapSize,
     width: miniMapSize,
     child: InteractiveViewer(
