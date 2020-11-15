80b1f82c7d867511223e77fbf6c21be680870453
teejstroyer <titoriano@gmail.com>
2020-11-15 09:33:26 -0600

    mini map is pannable, but not zoomable, offset now works as a function of size

--- lib/main.dart
+++ lib/main.dart
@@ -8,29 +8,34 @@ void main() {
   dung.generate(10);
   dung.rooms[Random().nextInt(dung.rooms.length)].current = true;
   dung.rooms[Random().nextInt(dung.rooms.length)].visited = true;
-  var squareSize = 90.0;
-  var currentRoom = dung.rooms.firstWhere((element) => element.current);
-  var mapOffset = Matrix4.translationValues(-(squareSize * currentRoom.x - squareSize / 2), -(squareSize * currentRoom.y - squareSize / 2), 0);
 
   runApp(
     MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Container(
-              decoration: BoxDecoration(border: Border.all(color: Colors.black), color: Colors.transparent),
-              height: 200,
-              width: 200,
-              child: InteractiveViewer(
-                transformationController: TransformationController(mapOffset),
-                boundaryMargin: EdgeInsets.all(0),
-                constrained: false,
-                scaleEnabled: true,
-                minScale: 0.01,
-                maxScale: 4,
-                child: UnconstrainedBox(clipBehavior: Clip.hardEdge, child: dung.drawGrid(squareSize)),
-              )),
+          child: getMiniMap(dung, 20),
         ),
       ),
+    ),
+  );
+}
+
+Container getMiniMap(Dungeon dung, double squareSize) {
+  var currentRoom = dung.rooms.firstWhere((element) => element.current);
+  double miniMapSize = 4 * squareSize;
+  double offsetConstant = 1.5 * squareSize;
+  var mapOffset = Matrix4.translationValues(-(squareSize * currentRoom.x - offsetConstant), -(squareSize * currentRoom.y - offsetConstant), 0);
+
+  return Container(
+    decoration: BoxDecoration(border: Border.all(color: Colors.black), color: Colors.transparent),
+    height: miniMapSize,
+    width: miniMapSize,
+    child: InteractiveViewer(
+      transformationController: TransformationController(mapOffset),
+      boundaryMargin: EdgeInsets.all(offsetConstant),
+      constrained: false,
+      scaleEnabled: false,
+      child: UnconstrainedBox(clipBehavior: Clip.hardEdge, child: dung.drawGrid(squareSize)),
     ),
   );
 }
